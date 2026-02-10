a0edebf1b746a3f780d3ffa00b10a51117dd8b51
PaulBeggs <paulbeggs03@gmail.com>
2026-02-10 15:36:05 -0600

    Nested the travel info retrieval inside the submitVehicle click function, and added console logs to verify that the correct coordinates are being retrieved for the start and end locations. So far, so good. Now, I need to get how long the trip takes, and the distance.

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,26 +1,5 @@
 $(document).ready(function() {
-  // Initally hide the vehicle selection div
-
-
-
   // Weather for traveling
-
-  var weatherUrl = "https://weather.open-meteo.com/v1/forecast?latitude=35.0887&longitude=-92.4421&hourly=temperature_2m&current=is_day,temperature_2m&timezone=America%2FChicago&forecast_days=3&temperature_unit=fahrenheit"
-  $('#submit').click(function () {
-    // Get current date's weather data up to 3 days into the future. based in conway
-    $.ajax({
-      url: weatherUrl,
-      dataType: 'json',
-      success: function(data) {
-        console.log(data)
-        // console.log(data["latitude"])
-      },
-      error: function(xhr, status, error) {
-        console.error("An error occured:", status, error)
-      }
-    });
-  });
-
 
 
   // City name to coordinates on the globe
@@ -44,6 +23,24 @@ $(document).ready(function() {
     });
   }
 
+  function getTravelInfo(startLat, startLon, endLat, endLon, vehicle) {
+    var hopperUrl = "https://graphhopper.com/api/1/route";
+    var route = {
+      "key": "8a4dfb56-a98a-4904-af80-4fcb0981c650",
+
+      // mode of transportation
+      "profile": vehicle,
+
+      "points": [[startLat, startLon],[endLat,endLon]],
+    };
+    return $.ajax({
+      url: hopperUrl,
+      traditional: true,
+      data: route,
+      dataType: "json"
+  });
+  }
+
   locationKey = "7f6e8eca58ccf6beb3f078f7dcfc33d5"
 
   $('#submitCity').click(function() {
@@ -52,8 +49,8 @@ $(document).ready(function() {
     var end = getCityState('#endCityBox')
     
     $.when(
-      // getCoordinates(start.city, start.state),
-      // getCoordinates(end.city, end.state)
+      getCoordinates(start.city, start.state),
+      getCoordinates(end.city, end.state)
     ).done(function(startData, endData) {
       
       // adds a label for the drop down menu 
@@ -61,7 +58,7 @@ $(document).ready(function() {
       $vehicleP.text("Select your mode of transportation: ")
 
       // adds a drop down menu that only has "Car", "Bike", or "Walking" to the vehicleSelection id
-      var $vehicleInput = $("<select id='vehicleSelect'><option value='car'>Car</option><option value='bike'>Bike</option><option value='walking'>Walking</option></select>")
+      var $vehicleInput = $("<select id='vehicleSelect'><option value='car'>Car</option><option value='bike'>Bike</option><option value='foot'>Walking</option></select>")
 
       // append a button next to the drop down menu to submit choice
       var $submitVehicle = $("<button id='submitVehicle'>Submit</button>")
@@ -77,13 +74,22 @@ $(document).ready(function() {
       var startLat = startData[0][0].lat;
       var startLon = startData[0][0].lon;
 
+      console.log("Start Lat: " + startLat + " Start Lon: " + startLon)
+
       var endLat = endData[0][0].lat;
       var endLon = endData[0][0].lon;
 
+      console.log("End Lat: " + endLat + " End Lon: " + endLon)
+
       // now that we have our coordinates, and a button set up to get the appropriate vehicle, we need to query the traffic api to get the distance of the trip, and then query the weather api to get the weather for the day of the trip.
-      $.when(
-        
-      )
+      $("#submitVehicle").click(function() {
+        var vehicle = $("#vehicleSelect").val();          
+        $.when(
+          getTravelInfo(startLat, startLon, endLat, endLon)
+        ).done(function(travelData) {
+          console.log(travelData)
+        });
+      });
 
       var weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude="+ endLat +"&longitude="+endLon+"&hourly=temperature_2m,wind_direction_120m,wind_speed_120m,precipitation,precipitation_probability&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
       // $.ajax({
@@ -102,36 +108,18 @@ $(document).ready(function() {
 
   apiKey = "8a4dfb56-a98a-4904-af80-4fcb0981c650";
   // Define the points for rloging as coordinates (lon, lat)
-  var rloge = {
-    "key": apiKey,
 
-    // mode of transportation
-    "profile": "car",
-    
-    // array of numbers (north first, then east)
-    "points": [
-      [
-        -11.539421,
-        -48.118477
-      ],
-      [
-        -11.559023,
-        -48.12228
-      ]
-    ],
-  };
     
   // Define the API endpoint URL
-  // var hopperUrl = "https://graphhopper.com/api/1/rloge";
  
   
   // $.ajax({
   //     url: hopperUrl,
   //     traditional: true,
-  //     data: rloge,
+  //     data: route,
   //     dataType: "json", 
   //     success: function(data) {
-  //         console.log("Rloging successful:", data);
+  //         console.log("Routing successful:", data);
   //     },
   //     error: function(status, error) {
   //       console.error("An error occured:", status, error)

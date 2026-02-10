28077c11792a38cee9b8cca13b58f0be5c62b4b9
PaulBeggs <paulbeggs03@gmail.com>
2026-02-10 14:49:29 -0600

    Figured out how to get coordinates from city name input and use those coordinates to get weather data for the destination city. Now, I am working on getting the vehicle selection to show up after the city input is submitted.

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  // Initally hide the vehicle selection div
+  $("#vehicleSelection").hide();
+
+  
 
   // Weather for traveling
-
 
   var weatherUrl = "https://weather.open-meteo.com/v1/forecast?latitude=35.0887&longitude=-92.4421&hourly=temperature_2m&current=is_day,temperature_2m&timezone=America%2FChicago&forecast_days=3&temperature_unit=fahrenheit"
   $('#submit').click(function () {
@@ -23,23 +26,60 @@ $(document).ready(function() {
 
   // City name to coordinates on the globe
 
-  locationKey = "7f6e8eca58ccf6beb3f078f7dcfc33d5"
-  stateCode = "MO"
-  cityName = "Kelso"
-  var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"," + stateCode  + "," + "US" + "&limit=1&appid=" + locationKey
-  $('#submit').click(function () {
-    // Get current date's weather data up to 3 days into the future. based in conway
-    $.ajax({
-      url: locationUrl,
-      dataType: 'json',
-      success: function(data) {
-        console.log(data)
-        // console.log(data["latitude"])
-      },
-      error: function(xhr, status, error) {
-        console.error("An error occured:", status, error)
-      }
+  function getCityState(selector) {
+    var input = $(selector).val();
+
+    var parts = input.split(",")
+
+    return {
+      city: parts[0] ? parts[0].trim() : "",
+      state: parts[1] ? parts[1].trim() : ""
+    };
+  };
+
+  function getCoordinates(city, state) {
+    return $.ajax({
+      url: "https://api.openweathermap.org/geo/1.0/direct?q=" 
+      + city + "," + state + ",US&limit=1&appid=" + locationKey,
+      dataType: 'json'
     });
+  }
+
+  locationKey = "7f6e8eca58ccf6beb3f078f7dcfc33d5"
+
+  $('#submitCity').click(function() {
+    // Get start location first
+    var start = getCityState('#startCityBox');
+    var end = getCityState('#endCityBox')
+    
+    $.when(
+      getCoordinates(start.city, start.state),
+      getCoordinates(end.city, end.state)
+    ).done(function(startData, endData) {
+      // console.log(startData, endData)
+
+      var now = new Date();
+      console.log(now)
+
+      var startLat = startData[0][0].lat;
+      var startLon = startData[0][0].lon;
+
+      var endLat = endData[0][0].lat;
+      var endLon = endData[0][0].lon;
+
+      $.when(
+        
+      )
+
+      var weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude="+ endLat +"&longitude="+endLon+"&hourly=temperature_2m,wind_direction_120m,wind_speed_120m,precipitation,precipitation_probability&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
+      $.ajax({
+        url: weatherUrl,
+        dataType: 'json',
+        success: function(data) {
+          console.log(data)
+        }
+      });
+    });  
   });
 
 
@@ -47,8 +87,8 @@ $(document).ready(function() {
 
 
   apiKey = "8a4dfb56-a98a-4904-af80-4fcb0981c650";
-  // Define the points for routing as coordinates (lon, lat)
-  var route = {
+  // Define the points for rloging as coordinates (lon, lat)
+  var rloge = {
     "key": apiKey,
 
     // mode of transportation
@@ -68,20 +108,20 @@ $(document).ready(function() {
   };
     
   // Define the API endpoint URL
-  var hopperUrl = "https://graphhopper.com/api/1/route";
+  // var hopperUrl = "https://graphhopper.com/api/1/rloge";
  
   
-  $.ajax({
-      url: hopperUrl,
-      traditional: true,
-      data: route,
-      dataType: "json", 
-      success: function(data) {
-          console.log("Routing successful:", data);
-      },
-      error: function(status, error) {
-        console.error("An error occured:", status, error)
-      }
-  });
+  // $.ajax({
+  //     url: hopperUrl,
+  //     traditional: true,
+  //     data: rloge,
+  //     dataType: "json", 
+  //     success: function(data) {
+  //         console.log("Rloging successful:", data);
+  //     },
+  //     error: function(status, error) {
+  //       console.error("An error occured:", status, error)
+  //     }
+  // });
 
 });
